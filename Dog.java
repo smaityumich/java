a004f0bfedc2c3318833e1c38253d5df0d5e8ff4
smaityumich <56092728+smaityumich@users.noreply.github.com>
2020-03-17 12:17:22 -0400

    new file:   DisplayOverloading.class new file:   Dog.java new file:   ExampleOverloading.class new file:   ExampleOverloading.java new file:   MathTeacher.class new file:   Teacher.class

---
+++ Dog.java
@@ -0,0 +1,16 @@
+class Animal {
+    public void animalSound(){
+        System.out.println("Default Sound");
+    }
+}
+
+public class Dog extends Animal {
+    public void animalSound(){
+        System.out.println("Woof");
+    }
+
+    public static void main(String[] args) {
+        Animal obj  = new Dog();
+        obj.animalSound();
+    }
+}
